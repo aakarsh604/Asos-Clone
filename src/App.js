4394175cad564491d3c7273c48a91d0a10a9fa9a
aakarsh604 <aakarsh604@gmail.com>
2022-06-15 11:34:31 +0530

    created menPage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Navigation from "./components/Navbar/Navbar";
 import Login from './pages/Login';
+import MenPage from "./pages/Menpage/MenPage"
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}></Route>
           <Route path='/login' element={<Login/>}></Route>
-        
+          <Route path="/menpage" element={ <MenPage/> } />
         </Routes>
     </div>
   );
