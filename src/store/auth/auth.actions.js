94b71a2393b645e38175d884122cdd92d80ebaab
rovin76 <rovin7678>
2022-06-16 14:48:40 +0530

    commit

--- src/store/auth/auth.actions.js
+++ src/store/auth/auth.actions.js
@@ -1,6 +1,16 @@
-import { LOGIN_SUCCESS, LOGOUT } from "./auth.types";
+import { LOGIN_ERROR, LOGIN_LOADING, LOGIN_SUCCESS, LOGOUT } from "./auth.types";
+import axios from "axios"
 
+export const loginReq = (payload) => (dispatch)=>{
+      // loading
+      dispatch({type:LOGIN_LOADING})
+      // success
+      axios.get("http://localhost:8080/users").then((res)=>{
+        console.log(res)
+        dispatch({type:LOGIN_SUCCESS,payload:payload})
+      }).catch((error)=>{
+        dispatch({type:LOGIN_ERROR,payload:error});
+      })
+}
 
-export const loginReq = () => ({type : LOGIN_SUCCESS, payload})
-
-export const logoutReq = () => ({type : LOGOUT, payload})
+export const logoutReq = () => ({type : LOGOUT})
